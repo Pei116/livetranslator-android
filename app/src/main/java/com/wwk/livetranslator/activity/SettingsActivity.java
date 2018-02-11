1a37d7202a866b45492810f42b53a4ecef9d44d2
Pei <peipeilong116@gmail.com>
2018-02-12 00:16:13 +0800

    - settings

---
+++ app/src/main/java/com/wwk/livetranslator/activity/SettingsActivity.java
@@ -0,0 +1,47 @@
+package com.wwk.livetranslator.activity;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.wwk.livetranslator.R;
+import com.wwk.livetranslator.fragment.SettingsFragment;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        getFragmentManager().beginTransaction()
+                .replace(R.id.contentLayout, new SettingsFragment())
+                .commit();
+
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
